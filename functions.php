78d70af1722483300fef58cb7f79e2fff2f51b49
nebaba <ratov-kirill84@yandex.ru>
2016-06-26 23:43:40 +0300

    Добавил конец второго урока, хлебные крошки

--- functions.php
+++ functions.php
@@ -1,61 +1,76 @@
 <?php
 
-/*
- * Распечатка массива
- * */
-function print_arr($array)
-{
-    echo "<pre>" . print_r($array, true) . "</pre>";
-}
-
-/*
- * Получение массива категорий
- * */
-function get_cat()
-{
-    global $connection;
-    $query = "SELECT * FROM categories";
-    $res = mysqli_query($connection, $query);
-
-    $arr_cat = array();
-    while($row = mysqli_fetch_assoc($res)) {
-        $arr_cat[$row['id']] = $row;
-    }
-    return $arr_cat;
+/**
+* Распечатка массива
+**/
+function print_arr($array){
+	echo "<pre>" . print_r($array, true) . "</pre>";
 }
 
 /**
- * Построение дерева
- **/
+* Получение массива категорий
+**/
+function get_cat(){
+	global $connection;
+	$query = "SELECT * FROM categories";
+	$res = mysqli_query($connection, $query);
+
+	$arr_cat = array();
+	while($row = mysqli_fetch_assoc($res)){
+		$arr_cat[$row['id']] = $row;
+	}
+	return $arr_cat;
+}
+
+/**
+* Построение дерева
+**/
 function map_tree($dataset) {
-    $tree = array();
+	$tree = array();
 
-    foreach ($dataset as $id=>&$node) {
-        if (!$node['parent']){
-            $tree[$id] = &$node;
-        }else{
+	foreach ($dataset as $id=>&$node) {    
+		if (!$node['parent']){
+			$tree[$id] = &$node;
+		}else{ 
             $dataset[$node['parent']]['childs'][$id] = &$node;
-        }
-    }
+		}
+	}
 
-    return $tree;
+	return $tree;
 }
 
 /**
- * Дерево в строку HTML
- **/
+* Дерево в строку HTML
+**/
 function categories_to_string($data){
-    foreach($data as $item){
-        $string .= categories_to_template($item);
-    }
-    return $string;
+	foreach($data as $item){
+		$string .= categories_to_template($item);
+	}
+	return $string;
 }
 
 /**
- * Шаблон вывода категорий
- **/
+* Шаблон вывода категорий
+**/
 function categories_to_template($category){
-    ob_start();
-    include 'category_template.php';
-    return ob_get_clean();
+	ob_start();
+	include 'category_template.php';
+	return ob_get_clean();
+}
+
+/**
+* Хлебные крошки
+**/
+function breadcrumbs($array, $id){
+	if(!$id) return false;
+
+	$count = count($array);
+	$breadcrumbs_array = array();
+	for($i = 0; $i < $count; $i++){
+		if($array[$id]){
+			$breadcrumbs_array[$array[$id]['id']] = $array[$id]['title'];
+			$id = $array[$id]['parent'];
+		}else break;
+	}
+	return array_reverse($breadcrumbs_array, true);
 }
--- index.php
+++ index.php
@@ -1,38 +1,33 @@
-<?php
-include "config.php";
-include "functions.php";
-
-$categories = get_cat();
-$categories_tree = map_tree($categories);
-$categories_menu = categories_to_string($categories_tree);
-?>
+<?php include 'catalog.php'; ?>
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Каталог</title>
-    <link rel="stylesheet" href="style.css">
+	<meta charset="UTF-8">
+	<title>Каталог</title>
+	<link rel="stylesheet" href="style.css">
 </head>
 <body>
-<a href="/catalog/">Главная</a>
-<div class="wrapper">
-    <div class="sidebar">
-        <ul class="category">
-            <?php echo $categories_menu; ?>
-        </ul>
-    </div>
-    <div class="content">
-        
-    </div>
-</div>
-
-<script src="js/jquery-1.9.0.min.js"></script>
-<script src="js/jquery.accordion.js"></script>
-<script src="js/jquery.cookie.js"></script>
-<script>
-    $(document).ready( function () {
-        $(".category").dcAccordion();
-    } );
-</script>
+	<a href="/catalog/">Главная</a>
+	<div class="wrapper">
+		<div class="sidebar">
+			<ul class="category">
+				<?php echo $categories_menu ?>
+			</ul>
+		</div>
+		<div class="content">
+			<p><?=$breadcrumbs;?></p>
+			<br>
+			<hr>
+			<?php print_arr($categories) ?>
+		</div>
+	</div>
+	<script src="js/jquery-1.9.0.min.js"></script>
+	<script src="js/jquery.accordion.js"></script>
+	<script src="js/jquery.cookie.js"></script>
+	<script>
+		$(document).ready(function(){
+			$(".category").dcAccordion();
+		});
+	</script>
 </body>
 </html>
