2a40fe93172c8088362ec43faa27ea2030967f2b
ratov <ratov-kirill84@yandex.ru>
2016-06-28 22:18:53 +0300

    Залил четвертый урок

--- functions.php
+++ functions.php
@@ -75,37 +75,57 @@ function breadcrumbs($array, $id){
 	return array_reverse($breadcrumbs_array, true);
 }
 
-/*
- * Получение ID дочерник категорий
- */
-function cats_id($array, $id)
-{
-    if(!$id) return false;
+/**
+* Получение ID дочерних категорий
+**/
+function cats_id($array, $id){
+	if(!$id) return false;
 
-    foreach($array as $item) {
-        if($item['parent'] == $id) {
-            $data .= $item['id'] . ",";
-            $data .= cats_id($array, $item['id']);
-        }
-    }
-
-    return $data;
+	foreach($array as $item){
+		if($item['parent'] == $id){
+			$data .= $item['id'] . ",";
+			$data .= cats_id($array, $item['id']);
+		}
+	}
+	return $data;
 }
 
-/*
- * Получение товаров
- */
-function get_products($ids = false){
-    global $connection;
-    if($ids){
-        $query = "SELECT * FROM products WHERE parent IN($ids) ORDER BY title";
-    }else{
-        $query = "SELECT * FROM products ORDER BY title";
-    }
-    $res = mysqli_query($connection, $query);
-    $products = array();
-    while($row = mysqli_fetch_assoc($res)){
-        $products[] = $row;
-    }
-    return $products;
+/**
+* Получение товаров
+**/
+function get_products($ids, $start_pos, $perpage){
+	global $connection;
+	if($ids){
+		$query = "SELECT * FROM products WHERE parent IN($ids) ORDER BY title LIMIT $start_pos, $perpage";
+	}else{
+		$query = "SELECT * FROM products ORDER BY title LIMIT $start_pos, $perpage";
+	}
+	$res = mysqli_query($connection, $query);
+	$products = array();
+	while($row = mysqli_fetch_assoc($res)){
+		$products[] = $row;
+	}
+	return $products;
+}
+
+/**
+* Кол-во товаров
+**/
+function count_goods($ids){
+	global $connection;
+	if( !$ids ){
+		$query = "SELECT COUNT(*) FROM products";
+	}else{
+		$query = "SELECT COUNT(*) FROM products WHERE parent IN($ids)";
+	}
+	$res = mysqli_query($connection, $query);
+	$count_goods = mysqli_fetch_row($res);
+	return $count_goods[0];
+}
+
+/**
+* Постраничная навигация
+**/
+function pagination($page, $count_pages){
+	return "Постраничная навигация";
 }
--- index.php
+++ index.php
@@ -2,38 +2,41 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Каталог</title>
-    <link rel="stylesheet" href="style.css">
+	<meta charset="UTF-8">
+	<title>Каталог</title>
+	<link rel="stylesheet" href="style.css">
 </head>
 <body>
-<a href="/catalog/">Главная</a>
-<div class="wrapper">
-    <div class="sidebar">
-        <ul class="category">
-            <?php echo $categories_menu ?>
-        </ul>
-    </div>
-    <div class="content">
-        <p><?=$breadcrumbs;?></p>
-        <br>
-        <hr>
-        <?php if($products): ?>
-            <?php foreach($products as $product): ?>
-                <a href="?product=<?=$product['id']?>"><?=$product['title']?></a><br>
-            <?php endforeach; ?>
-        <?php else: ?>
-            <p>Здесь товаров нет!</p>
-        <?php endif; ?>
-    </div>
-</div>
-<script src="js/jquery-1.9.0.min.js"></script>
-<script src="js/jquery.accordion.js"></script>
-<script src="js/jquery.cookie.js"></script>
-<script>
-    $(document).ready(function(){
-        $(".category").dcAccordion();
-    });
-</script>
+	<a href="/catalog/">Главная</a>
+	<div class="wrapper">
+		<div class="sidebar">
+			<ul class="category">
+				<?php echo $categories_menu ?>
+			</ul>
+		</div>
+		<div class="content">
+			<p><?=$breadcrumbs;?></p>
+			<br>
+			<hr>
+			<?php if($products): ?>
+
+				<div class="pagination"><?=$pagination?></div>
+
+				<?php foreach($products as $product): ?>
+					<a href="?product=<?=$product['id']?>"><?=$product['title']?></a><br>
+				<?php endforeach; ?>
+			<?php else: ?>
+				<p>Здесь товаров нет!</p>
+			<?php endif; ?>
+		</div>
+	</div>
+	<script src="js/jquery-1.9.0.min.js"></script>
+	<script src="js/jquery.accordion.js"></script>
+	<script src="js/jquery.cookie.js"></script>
+	<script>
+		$(document).ready(function(){
+			$(".category").dcAccordion();
+		});
+	</script>
 </body>
 </html>
